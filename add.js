65b9687ab2419f06708a5eafc357bb49876d9ef6
Aakash Chaudhary <aakash.199303185@muj.manipal.edu>
2021-08-29 11:28:44 +0530

    fixed shit

--- add.js
+++ add.js
@@ -1,5 +1,3 @@
 function publish(){
-
-    var code = document.querySelector(".gjs-dashed").textContent;
-    document.querySelector("#content").textContent += code;
+    editor.load(res => console.log('Load callback'));
 }
